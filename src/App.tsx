deb0b8ca5bc41f58fb55156ed95009933a75e16b
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-24 17:00:30 +0300

    microtask button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
 import './App.css';
+import {Button} from './components/Button';
 
 function App() {
+    const button1Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
+    }
+    const button2Foo = () => {
+        console.log('Im Igor')
+    }
+    const button3Foo = () => {
+        console.log('Im stupid button')
+    }
     return (
         <div className="App">
-
+            {/*<button>myYouTubeChanel-1</button>*/}
+            {/*<button>myYouTubeChanel-2</button>*/}
+            <Button name={'myYouTubeChanel-1'} callBack={() => button1Foo('Im Vasya', 21)}/>
+            <Button name={'myYouTubeChanel-2'} callBack={button2Foo}/>
+            <Button name={'stupidButton'} callBack={button3Foo}/>
         </div>
     );
 }
